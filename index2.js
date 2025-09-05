81324cffcd6717f97026947662e37ed5c3f53a25
yashtaken <yashraj25597@gmail.com>
2025-09-05 23:18:58 +0530

    Delete index2.js

--- index2.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("copy", function(event) {
-     event.preventDefault();
-     document.getElementById("content").innerHTML = "";
-      alert("Logged out as unfair means detected");
-     });
